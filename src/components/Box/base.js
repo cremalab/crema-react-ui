4d06d23b8e465d00bdeed804833b1d8fed630e30
Rob LaFeve <rob@cremalab.com>
2017-02-03 14:54:59 -0600

    Box stuff

--- src/components/Box/base.js
+++ src/components/Box/base.js
@@ -1,13 +1,27 @@
 import React, { PropTypes } from 'react'
 
-const BoxBase = ({ El }) => ({ children }) => (
-  <El>
-    { children }
-  </El>
-)
-
-BoxBase.propTypes = {
-  chidlren: PropTypes
+const Base = (props) => {
+  const { deps: { Container, Inner, platform }, children } = props
+  const isString = typeof children === 'string'
+  const isNative = platform === 'native'
+  return (
+    <Container>
+      {
+        isNative && isString
+        ? <Inner>{children}</Inner>
+        : children
+      }
+    </Container>
+  )
 }
 
-export default boxBase
+Base.displayName = 'Box'
+Base.propTypes = {
+  children: PropTypes.any,
+  deps: PropTypes.object
+}
+Base.defaultProps = {
+  deps: {}
+}
+
+export default Base
--- src/components/Box/web.js
+++ src/components/Box/web.js
@@ -1,3 +1,16 @@
-import boxBase from './base'
-import web from 'styled-components'
-export default boxBase({ El: web.div`` })
+import React from 'react'
+import Base from './Base'
+import styled from 'styled-components'
+
+const div = styled.div`
+  background: red
+`
+
+const deps = {
+  platform: 'web',
+  Container: div
+}
+
+export default function Web(props) {
+  return <Base deps={deps} {...props} />
+}
